b922a89c6c1f04c62b198f2eb097e3f0a9e54846
Dara Anya <daraanya@gmail.com>
2019-04-23 20:40:25 -0500

    Updates

---
+++ precache-manifest.17fb72c7c9b1eea7dd67263963b3a2e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "94edda8917063d1bab31",
+    "url": "/Clicky-Game/static/js/main.41b06143.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "94edda8917063d1bab31",
+    "url": "/Clicky-Game/static/css/main.2256389d.chunk.css"
+  },
+  {
+    "revision": "aaa355cf3477148a09c5f8d601a32574",
+    "url": "/Clicky-Game/index.html"
+  }
+];
